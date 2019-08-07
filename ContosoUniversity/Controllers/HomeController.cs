6b5b55cc0c18eb3f1fb6e0c5dd3022e0148d0838
mannharjinder <48392420+mannharjinder@users.noreply.github.com>
2019-08-07 22:17:50 +1200

    week 3

--- ContosoUniversity/Controllers/HomeController.cs
+++ ContosoUniversity/Controllers/HomeController.cs
@@ -5,21 +5,46 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore; //The following three lines were added in Week 3
+using ContosoUniversity.Data;
+using ContosoUniversity.Models.SchoolViewModels;
 
 namespace ContosoUniversity.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SchoolContext _context; //Added in Week 3 
+        public HomeController(SchoolContext context) //Added in Week 3
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
+        //Replaced in Week 3
 
-            return View();
+        //public IActionResult About()
+        //{
+        //    ViewData["Message"] = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //Added in Week 3
+        public async Task<ActionResult> About()
+        {
+            IQueryable<EnrollmentDateGroup> data =
+            from student in _context.Students
+            group student by student.EnrollmentDate into dateGroup
+            select new EnrollmentDateGroup()
+            {
+                EnrollmentDate = dateGroup.Key,
+                StudentCount = dateGroup.Count()
+            };
+            return View(await data.AsNoTracking().ToListAsync());
         }
 
         public IActionResult Contact()
